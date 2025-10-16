deed9655387eb423e1b331da3aa82afdb1a9e7b3
gael <gmarcelinodocio@danielcastelao.org>
2025-10-16 13:27:05 +0200

    Ejercicio2

---
+++ Boletin3/ejercicio2.py
@@ -0,0 +1,11 @@
+numero1 = int(input("Introduce el primero numero"))
+numero2 = int(input("Introduce el segundo numero"))
+
+if numero1 > numero2:
+    resta = numero1-numero2
+    print ("La resta es:" )
+    print (resta)
+
+    suma = numero1 + numero2
+    print ("La suma es:")
+    print (suma)
