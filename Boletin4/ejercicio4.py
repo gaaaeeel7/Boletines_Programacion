57cadc392a7c298f0904ca87cf8ac06e4adc373b
gael <gmarcelinodocio@danielcastelao.org>
2025-10-16 13:36:14 +0200

    Ejercicio4

---
+++ Boletin4/ejercicio4.py
@@ -0,0 +1,207 @@
+numero = int(input("Dame un numero: "))
+
+if numero == 1:
+    print ("uno")
+if numero == 2:
+    print("dos")
+if numero == 3:
+    print("tres")
+if numero == 4:
+    print("cuatro")
+if numero == 5:
+    print("cinco")
+if numero == 6:
+    print("seis")
+if numero == 7:
+    print("siete")
+if numero == 8:
+    print("ocho")
+if numero == 9:
+    print("nueve")
+if numero == 10:
+    print("diez")
+if numero == 11:
+    print("once")
+if numero == 12:
+    print("doce")
+if numero == 13:
+    print("trece")
+if numero == 14:
+    print("catorce")
+if numero == 15:
+    print("quince")
+if numero == 16:
+    print("dieciseis")
+if numero == 17:
+    print("diecisiete")
+if numero == 18:
+    print("dieciocho")
+if numero == 19:
+    print("diecinueve")
+if numero == 20:
+    print("veinte")
+if numero == 21:
+    print("veinte y una")
+if numero == 22:
+    print("veinte y dos")
+if numero == 23:
+    print("veinte y tres")
+if numero == 24:
+    print("veinte y cuatro")
+if numero == 25:
+    print("veinte y cinco")
+if numero == 26:
+    print("veinte y seis")
+if numero == 27:
+    print("veinte y siete")
+if numero == 28:
+    print("veinte y ocho")
+if numero == 29:
+    print("veinte y nueve")
+if numero == 30:
+    print("treinta")
+if numero == 31:
+    print ("treinta y uno")
+if numero == 32:
+    print ("treinta y dos")
+if numero == 33:
+    print ("treinta y tres")
+if numero == 34:
+    print ("treinta y cuatro")
+if numero == 35:
+    print ("treinta y cinco")
+if numero == 36:
+    print ("treinta y seis")
+if numero == 37:
+    print ("treinta y siete")
+if numero == 38:
+    print ("treinta y ocho")
+if numero == 39:
+    print ("treinta y nueve")
+if numero == 40:
+    print ("cuarenta")
+if numero == 41:
+    print ("cuarenta y uno")
+if numero == 42:
+    print ("cuarenta y dos")
+if numero == 43:
+    print ("cuarenta y tres")
+if numero == 44:
+    print ("cuarenta y cuatro")
+if numero == 45:
+    print ("cuarenta y cinco")
+if numero == 46:
+    print ("cuarenta y seis")
+if numero == 47:
+    print ("cuarenta y siete")
+if numero == 48:
+    print ("cuarenta y ocho")
+if numero == 49:
+    print ("cuarenta y nueve")
+if numero == 50:
+    print ("cincuenta")
+if numero == 51:
+    print ("cincuenta y uno")
+if numero == 52:
+    print ("cincuenta y dos")
+if numero == 53:
+    print ("cincuenta y tres")
+if numero == 54:
+    print ("cincuenta y cuatro")
+if numero == 55:
+    print ("cincuenta y cinco")
+if numero == 56:
+    print ("cincuenta y seis")
+if numero == 57:
+    print ("cincuenta y siete")
+if numero == 58:
+    print ("cincuenta y ocho")
+if numero == 59:
+    print ("cincuenta y nueve")
+if numero == 60:
+    print ("sesenta")
+if numero == 61:
+    print ("sesenta y uno")
+if numero == 62:
+    print ("sesenta y dos")
+if numero == 63:
+    print ("sesenta y tres")
+if numero == 64:
+    print ("sesenta y cuatro")
+if numero == 65:
+    print ("sesenta y cinco")
+if numero == 66:
+    print ("sesenta y seis")
+if numero == 67:
+    print ("sesenta y siete")
+if numero == 68:
+    print ("sesenta y ocho")
+if numero == 69:
+    print ("sesenta y nueve")
+if numero == 70:
+    print ("setenta")
+if numero == 71:
+    print ("setenta y uno")
+if numero == 72:
+    print ("setenta y dos")
+if numero == 73:
+    print ("setenta y tres")
+if numero == 74:
+    print ("setenta y cuatro")
+if numero == 75:
+    print ("setenta y cinco")
+if numero == 76:
+    print ("setenta y seis")
+if numero == 77:
+    print ("setenta y siete")
+if numero == 78:
+    print ("setenta y ocho")
+if numero == 79:
+    print ("setenta y nueve")
+if numero == 80:
+    print ("ochenta")
+if numero == 81:
+    print ("ochenta y uno")
+if numero == 82:
+    print ("ochenta y dos")
+if numero == 83:
+    print ("ochenta y tres")
+if numero == 84:
+    print("ochenta y cuatro")
+if numero == 85:
+    print ("ochenta y cinco")
+if numero == 86:
+    print("ochenta y seis")
+if numero == 87:
+    print ("ochenta y siete")
+if numero == 88:
+    print ("ochenta y ocho")
+if numero == 89:
+    print ("ochenta y nueve")
+if numero == 90:
+    print ("noventa")
+if numero == 91:
+    print ("noventa y uno")
+if numero == 92:
+    print ("noventa y dos")
+if numero == 93:
+    print ("noventa y tres")
+if numero == 94:
+    print ("noventa y cuatro")
+if numero == 95:
+    print ("noventa y cinco")
+if numero == 96:
+    print ("noventa y seis")
+if numero == 97:
+    print ("noventa y siete")
+if numero == 98:
+    print ("noventa y ocho")
+if numero == 99:
+    print ("noventa y nueve")
+if numero == 100:
+    print ("cien")
+
+
+else:
+    if numero >100:
+        print ("No puede ser")
