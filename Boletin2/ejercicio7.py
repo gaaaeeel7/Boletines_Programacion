2152809b319ce671d1abb4133a3c9c0fbd56645a
gael <gmarcelinodocio@danielcastelao.org>
2025-10-17 09:29:11 +0200

    Ejercicio7

---
+++ Boletin2/ejercicio7.py
@@ -0,0 +1,8 @@
+grado_centigrados = int(input("Dame la temperatura"))
+
+fahrenheit = (grado_centigrados * 9/5) + 32
+kelvin = grado_centigrados + 273.15
+
+print ("Fahrenheit: " , fahrenheit)
+print ("Kelvin: " , kelvin)
+
