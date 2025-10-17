520c94f58de7dcb0996e6f43ce7e3cfdc80f193a
gael <gmarcelinodocio@danielcastelao.org>
2025-10-17 09:04:23 +0200

    Ejercicio4

---
+++ Boletin2/ejercicio4.py
@@ -0,0 +1,15 @@
+numero_1 = int(input("Dame un numero"))
+numero_2 = int(input("Dame otro numero"))
+
+
+
+
+suma = numero_1 + numero_2
+resta = numero_1 - numero_2
+producto = numero_1 * numero_2
+cociente = numero_1 / numero_2
+
+print ("Este es el resultado de la suma" , suma)
+print ("Este es el resultado de la resta" , resta)
+print ("Este es el resultado de la multiplicacion: " , producto)
+print ("Este es el resultado de la division" , cociente)
