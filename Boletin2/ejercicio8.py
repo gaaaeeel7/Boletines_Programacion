593ff6e920e2940dfadd2520a6f0f4fb072934c9
gael <gmarcelinodocio@danielcastelao.org>
2025-10-17 09:46:33 +0200

    Ejercicio8

---
+++ Boletin2/ejercicio8.py
@@ -0,0 +1,18 @@
+billetes_100 = int(input("Introduce el numero de billetes de 100: "))
+billetes_20 = int(input("Introduce el numero de billetes de 20 :"))
+billetes_5 = int(input("Introduce el numero de billetes de 5 :"))
+billetes_1 = int(input("Introduce el numero de billetes de 1 :"))
+
+
+total_1 = 100* 2
+total_2 = 20 * 3
+total_3 = 5 * 0
+total_4 = 1 * 6
+
+print ("El numero de billetes de 100 es: " , total_1)
+print ("El numero de billetes de 20 es :" , total_2)
+print ("El numero de billetes de 5 es :" , total_3)
+print ("El numero de billetes de 1 es :" , total_4)
+
+print ("El numero de billetes es: ")
+print (total_1 + total_2 + total_3 + total_4)
