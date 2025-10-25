160930ae424198d84516ae7b124066f5ebb1fe64
gael <gmarcelinodocio@danielcastelao.org>
2025-10-25 14:38:25 +0200

    ejercicio9

---
+++ Boletin6/ejercicio9.py
@@ -0,0 +1,5 @@
+vector1 = [ 1,2,3]
+vector2 = [-1,0,2]
+
+producto_escalar = vector1[0] * vector2[0] + vector1[0] * vector2[1] + vector1[2] * vector2[2]
+print (f"El producto escalar es : {producto_escalar}")
