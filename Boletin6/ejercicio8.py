e31dd00e3b1c715bc42e407281e51ccf201024a4
gael <gmarcelinodocio@danielcastelao.org>
2025-10-25 14:38:10 +0200

    ejercicio8

---
+++ Boletin6/ejercicio8.py
@@ -0,0 +1,7 @@
+precio = [50,75,46,22,80,65,8]
+
+menor = min(precio)
+mayor = max(precio)
+
+print (f"Este es el que tiene mayor precio: {mayor}")
+print (f"Este es el que tiene menor precio: {menor}")
