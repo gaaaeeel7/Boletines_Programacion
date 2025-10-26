8a5c035bf9a8500e2e62343539ca4a8014925f79
gael <gmarcelinodocio@danielcastelao.org>
2025-10-26 12:57:46 +0100

    ejercicio8 modificado

--- Boletin2/ejercicio8.py
+++ Boletin2/ejercicio8.py
@@ -9,10 +9,7 @@ total_2 = 20 * 3
 total_3 = 5 * 0
 total_4 = 1 * 6
 
-print ("El numero de billetes de 100 es: " , total_1)
-print ("El numero de billetes de 20 es :" , total_2)
-print ("El numero de billetes de 5 es :" , total_3)
-print ("El numero de billetes de 1 es :" , total_4)
+
 
 print ("El numero de billetes es: ")
 print (total_1 + total_2 + total_3 + total_4)
