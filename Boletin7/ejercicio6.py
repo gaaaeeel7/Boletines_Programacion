3606667a1b3f13dcdc7fea6491607572f805f7c3
gael <gmarcelinodocio@danielcastelao.org>
2025-10-26 11:37:44 +0100

    ejercicio6

---
+++ Boletin7/ejercicio6.py
@@ -0,0 +1,13 @@
+cadena = "www.phytonparatodos.com"
+
+parte_1 = "www.phyton"
+parte_2 = "paratodos.com"
+
+print ("parte1: " , parte_1)
+print ("parte2: " , parte_2)
+
+
+concatena = parte_1 + parte_2
+
+print ("Muestra de nuevo la cadena: ", concatena)
+
