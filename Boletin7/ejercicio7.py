01e9e5ca737ac4eba16fca9ecad5d1df6fbdac3d
gael <gmarcelinodocio@danielcastelao.org>
2025-10-26 11:57:18 +0100

    ejercicio7

---
+++ Boletin7/ejercicio7.py
@@ -0,0 +1,9 @@
+texto = "phytoneros"
+
+texto_mayuscula = texto.upper()
+
+print("La palabra es: ", texto_mayuscula)
+
+texto_minuscula = texto.lower()
+
+print ("La palabra es: " , texto_minuscula)
