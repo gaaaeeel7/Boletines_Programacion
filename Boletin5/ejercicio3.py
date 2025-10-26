589d77dd5abfb45b82cf953a80a7d1f3a324bc87
gael <gmarcelinodocio@danielcastelao.org>
2025-10-26 20:15:09 +0100

    ejercicio3

--- Boletin5/ejercicio3.py
+++ Boletin5/ejercicio3.py
@@ -1,4 +1,8 @@
-print("Fahrenheit    Celsius")
-for f in range(0, 121, 10):
-    c = (f - 32) * 5 / 9
-    print(f"{f}, {c}")
+
+temperatura = 0
+temperaturas = (0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120)
+
+for x in temperaturas:
+    conversion = (9/5 * temperatura) + 32
+    temperatura += 10
+    print(conversion)
