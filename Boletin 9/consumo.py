c4dca9644bda811b093d5f5cc705e7fc855ec797
gael <gmarcelinodocio@danielcastelao.org>
2025-12-09 17:07:37 +0100

    ejerciciosPOO (consumo)

---
+++ Boletin 9/consumo.py
@@ -0,0 +1,36 @@
+class Consumo:
+    def __init__(self, km=0.0, litros=0.0, vMed=0.0, pGas=0.0):
+
+        self.km = km
+        self.litros = litros
+        self.vMed = vMed
+        self.pGas = pGas
+
+    def getTempo(self):
+        if self.vMed > 0:
+            return self.km / self.vMed
+        return 0
+
+    def consumoMedio(self):
+        if self.km > 0:
+            return (self.litros / self.km) * 100
+        return 0
+
+    def consumoEuros(self):
+        return self.consumoMedio() * self.pGas
+
+    def setKms(self, km):
+        self.km = km
+
+    def setLitros(self, litros):
+        self.litros = litros
+
+    def setvMed(self, vMed):
+        self.vMed = vMed
+
+
+if __name__ == "__main__":
+    coche = Consumo(km=150, litros=10, vMed=75, pGas=1.7)
+    print("Tempo da viaxe:", coche.getTempo(), "horas")
+    print("Consumo medio:", coche.consumoMedio(), "L/100km")
+    print("Consumo en euros:", coche.consumoEuros(), "€/100km")
